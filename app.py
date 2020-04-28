7536dfa84224baac362aa0b7500d61d26671b596
samadhi <suchait.mattoo010292@gmail.com>
2020-04-28 15:01:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,42 @@ app = Flask(__name__)
 def hello_world():
     return jsonify(response='Hello World!')
 
+
+@app.after_request
+def after_request(response):
+    """ Logging after every request. """
+    # This avoids the duplication of registry in the log,
+    # since that 500 is already logged via @app.errorhandler.
+    if response.status_code != 500:
+        ts = strftime('[%Y-%b-%d %H:%M]')
+        logger.error('%s %s %s %s %s %s',
+                      ts,
+                      request.remote_addr,
+                      request.method,
+                      request.scheme,
+                      request.full_path,
+                      response.status)
+    return response
+
+
+@app.errorhandler(Exception)
+def exceptions(e):
+    """ Logging after every Exception. """
+    ts = strftime('[%Y-%b-%d %H:%M]')
+    tb = traceback.format_exc()
+    logger.error('%s %s %s %s %s 5xx INTERNAL SERVER ERROR\n%s',
+                  ts,
+                  request.remote_addr,
+                  request.method,
+                  request.scheme,
+                  request.full_path,
+                  tb)
+    return "Internal Server Error", 500
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=3)        
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.ERROR)
+    logger.addHandler(handler)
+    app.run(host="127.0.0.1",port=8000)
